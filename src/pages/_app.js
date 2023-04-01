5902c09dc83f90b5026e62f4f9e41c8b4e5fb8ac
Paulo Torrealba <paulo@buscalibre.com>
2023-04-01 04:39:36 -0300

    fix problemas linter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,5 +2,6 @@ import "../styles/global.css";
 // import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+	// eslint-disable-next-line react/jsx-props-no-spreading
 	return <Component {...pageProps} />;
 }
